23b9230d7fb138dc644ead1d6624fd75ab8383a3
Thomas <tpienne51@gmail.com>
2020-06-06 08:52:00 +0200

    Les out

--- src/Appli.java
+++ src/Appli.java
@@ -2,6 +2,7 @@ public class Appli {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(() -> {
             System.out.println("toto");
+
             /*Model model = new Model();
             new ControlGroup(model);*/
         });
